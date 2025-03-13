f39cf9162375b49eeecc274213ff75b7486f6a8c
Antonio <pisouza011@gmail.com>
2025-03-12 22:28:35 -0300

    wip: Starting to work on entities, first one was Teacher

---
+++ src/main/java/com/antoniosousa/school/domain/model/Teacher.java
@@ -0,0 +1,62 @@
+package com.antoniosousa.school.domain.model;
+
+import com.antoniosousa.school.domain.enums.ContractType;
+import com.antoniosousa.school.domain.enums.EmployeeStatus;
+import jakarta.persistence.*;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "TB_TEACHER")
+@Data
+public class Teacher {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+    @Column(nullable = false, length = 50)
+    private String firstName;
+    @Column(nullable = false, length = 50)
+    private String lastName;
+    private String fullName;
+    @Column(nullable = false, unique = true, length = 18)
+    private String cpf;
+    @Column(nullable = false, unique = true, length = 60)
+    private String email;
+    private LocalDate birthDate;
+    @Column(nullable = false, unique = true, length = 18)
+    private String phone;
+    private LocalDate hireDate;
+
+    @Enumerated(EnumType.STRING)
+    private EmployeeStatus status;
+
+    @Enumerated(EnumType.STRING)
+    private ContractType contractType;
+
+    private BigDecimal salary;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+
+
+
+
+
+
+
+
+    @PrePersist
+    @PreUpdate
+    private void generateFullName() {
+        this.fullName = firstName + " " + lastName;
+    }
+}
