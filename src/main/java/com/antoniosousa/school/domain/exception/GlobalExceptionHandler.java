4cc80d5b57fbe85778d9fef1fac15093ac3af385
Antonio <pisouza011@gmail.com>
2025-03-16 16:50:12 -0300

    feat: added InvalidCepFormatException on GlobalExceptionHandler

--- src/main/java/com/antoniosousa/school/domain/exception/GlobalExceptionHandler.java
+++ src/main/java/com/antoniosousa/school/domain/exception/GlobalExceptionHandler.java
@@ -1,6 +1,8 @@
 package com.antoniosousa.school.domain.exception;
 
 
+import com.antoniosousa.school.domain.exception.cep.CepNotFoundException;
+import com.antoniosousa.school.domain.exception.cep.InvalidCepFormatException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -12,6 +14,11 @@ public class GlobalExceptionHandler {
     @ExceptionHandler({CepNotFoundException.class})
     public ResponseEntity<String> handleCepNotFound(CepNotFoundException ex) {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    }
+
+    @ExceptionHandler({InvalidCepFormatException.class})
+    public ResponseEntity<String> handleInvalidCepFormat(InvalidCepFormatException ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
     }
 
 }
