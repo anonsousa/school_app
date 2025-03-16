4c8493c27d25a82e2c43c9618212caee62371138
Antonio <pisouza011@gmail.com>
2025-03-16 16:48:02 -0300

    fix: fixing viewCep, check format added and checking response != null avoiding NullException

--- src/main/java/com/antoniosousa/school/domain/service/ViaCepService.java
+++ src/main/java/com/antoniosousa/school/domain/service/ViaCepService.java
@@ -1,7 +1,8 @@
 package com.antoniosousa.school.domain.service;
 
 import com.antoniosousa.school.domain.dto.ViaCepResponseDto;
-import com.antoniosousa.school.domain.exception.CepNotFoundException;
+import com.antoniosousa.school.domain.exception.cep.CepNotFoundException;
+import com.antoniosousa.school.domain.exception.cep.InvalidCepFormatException;
 import com.antoniosousa.school.integration.ViaCepClient;
 import org.springframework.stereotype.Service;
 
@@ -15,10 +16,19 @@ public class ViaCepService {
     }
 
     public ViaCepResponseDto viewCep(String cep) {
+        checkCepFormat(cep);
+
         ViaCepResponseDto response = viaCepClient.viewCep(cep);
-        if (response == null) {
+        if (response == null || (response.city() == null && response.state() == null)) {
             throw new CepNotFoundException("CEP not found: " + cep);
         }
         return response;
+    }
+
+
+    private void checkCepFormat(String cep) {
+        if (!cep.matches("\\d{8}")) {
+            throw new InvalidCepFormatException("Invalid CEP format, must have 8 digits without spaces and symbols.");
+        }
     }
 }
