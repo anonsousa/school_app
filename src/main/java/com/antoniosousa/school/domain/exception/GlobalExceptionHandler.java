4535e79c253aef199e82fd98b2185f16d87b2430
Antonio <pisouza011@gmail.com>
2025-03-16 17:36:02 -0300

    chore: add @Hidden to no conflict with Swagger

--- src/main/java/com/antoniosousa/school/domain/exception/GlobalExceptionHandler.java
+++ src/main/java/com/antoniosousa/school/domain/exception/GlobalExceptionHandler.java
@@ -3,12 +3,14 @@ package com.antoniosousa.school.domain.exception;
 
 import com.antoniosousa.school.domain.exception.cep.CepNotFoundException;
 import com.antoniosousa.school.domain.exception.cep.InvalidCepFormatException;
+import io.swagger.v3.oas.annotations.Hidden;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
+@Hidden /** I use this to solve problem on Swagger Start**/
 public class GlobalExceptionHandler {
 
     @ExceptionHandler({CepNotFoundException.class})
@@ -20,5 +22,4 @@ public class GlobalExceptionHandler {
     public ResponseEntity<String> handleInvalidCepFormat(InvalidCepFormatException ex) {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
     }
-
 }
