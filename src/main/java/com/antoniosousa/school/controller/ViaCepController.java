00170756d2826d7230c88e7ad405370c6294713e
Antonio <pisouza011@gmail.com>
2025-03-16 17:37:31 -0300

    chore: Added Information on Endpoint to use on Swagger as codes and summary

--- src/main/java/com/antoniosousa/school/controller/ViaCepController.java
+++ src/main/java/com/antoniosousa/school/controller/ViaCepController.java
@@ -1,8 +1,10 @@
 package com.antoniosousa.school.controller;
 
-
 import com.antoniosousa.school.domain.dto.ViaCepResponseDto;
 import com.antoniosousa.school.domain.service.ViaCepService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,6 +21,12 @@ public class ViaCepController {
         this.viaCepService = viaCepService;
     }
 
+    @Operation(summary = "Search for specific CEP", description = "Return CEP data using API ViaCEP.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "CEP successfully returned"),
+            @ApiResponse(responseCode = "400", description = "CEP not found"),
+            @ApiResponse(responseCode = "404", description = "Bad Request (as invalid CEP format)")
+    })
     @GetMapping("/{cep}")
     public ResponseEntity<ViaCepResponseDto> viewCep(@PathVariable("cep") String cep) {
         return ResponseEntity.ok().body(viaCepService.viewCep(cep));
