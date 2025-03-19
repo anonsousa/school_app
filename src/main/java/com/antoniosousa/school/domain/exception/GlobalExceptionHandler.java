28a2a810c978b5fc3143fbcdd8ad1030a5d8afa5
Antonio <pisouza011@gmail.com>
2025-03-18 22:27:48 -0300

    feat: add MethodArgumentNotValidException handler

--- src/main/java/com/antoniosousa/school/domain/exception/GlobalExceptionHandler.java
+++ src/main/java/com/antoniosousa/school/domain/exception/GlobalExceptionHandler.java
@@ -6,12 +6,31 @@ import com.antoniosousa.school.domain.exception.cep.InvalidCepFormatException;
 import io.swagger.v3.oas.annotations.Hidden;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestControllerAdvice
-@Hidden /** I use this to solve problem on Swagger Start**/
+@Hidden
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+
+        BindingResult bindingResult = ex.getBindingResult();
+
+        for (FieldError fieldError : bindingResult.getFieldErrors()) {
+            errors.put(fieldError.getField(), fieldError.getDefaultMessage());
+        }
+
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+    }
 
     @ExceptionHandler({CepNotFoundException.class})
     public ResponseEntity<String> handleCepNotFound(CepNotFoundException ex) {
