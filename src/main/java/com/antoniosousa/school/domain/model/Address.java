c7f83b28820dbfa4cf81b7dc4188328254fae09f
Antonio <pisouza011@gmail.com>
2025-03-18 22:25:00 -0300

    feat: add validation constraints to Address entity

--- src/main/java/com/antoniosousa/school/domain/model/Address.java
+++ src/main/java/com/antoniosousa/school/domain/model/Address.java
@@ -1,5 +1,6 @@
 package com.antoniosousa.school.domain.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -14,9 +15,14 @@ public class Address {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false, length = 80)
     private String street;
+    @Column(nullable = false, length = 12)
     private String number;
+    @Column(nullable = false, length = 60)
     private String city;
+    @Column(nullable = false, length = 30)
     private String state;
     @Digits(integer = 9, fraction = 0)
     private String zip;
